e0805758190bd9303638fafa1cf3e91811f02ccc
yvngmarti <ariasmartin.am@gmail.com>
2025-12-09 14:51:27 -0500

    [update] api name and version update

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -1,6 +1,6 @@
 class Constants:
-    PROJECT_NAME: str = "AYS API"
-    PROJECT_VERSION: str = "0.0.1"
+    PROJECT_NAME: str = "API Gestión de Gastos y Pagos de Obras (GGPO)"
+    PROJECT_VERSION: str = "0.4.0"
     API_PREFIX: str = "/api/v1"
 
 
